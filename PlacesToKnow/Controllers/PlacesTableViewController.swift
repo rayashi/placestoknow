be0c83e660399811605913bb71dffe563cb8ad1d
Humberto Dib Rayashi <rayashi@gmail.com>
2019-04-15 20:14:03 -0300

    Remove place, show all saved places

--- PlacesToKnow/Controllers/PlacesTableViewController.swift
+++ PlacesToKnow/Controllers/PlacesTableViewController.swift
@@ -11,20 +11,43 @@ class PlacesTableViewController: UITableViewController {
         self.loadPlaces()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.prepareNoItems()
+        self.prepareShowAllBtn()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case "segueToFind":
             let finderViewController = segue.destination as! FinderViewController
             finderViewController.delegate = self
         case "segueToMap":
+            let mapViewController = segue.destination as! MapViewController
             if let place = sender as! Place? {
-                let mapViewController = segue.destination as! MapViewController
                 mapViewController.places = [place]
+                mapViewController.title = place.name
+            } else {
+                mapViewController.places = places
+                mapViewController.title = "Meus Locais"
             }
         default:
             return
         }
-
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.places.remove(at: indexPath.row)
+            self.savePlaces()
+        }
+    }
+    
+    func savePlaces() {
+        let json = try? JSONEncoder().encode(self.places)
+        UserDefaults.standard.set(json, forKey: "places")
+        self.tableView.reloadData()
+        self.prepareNoItems()
+        self.prepareShowAllBtn()
     }
     
     func loadPlaces() {
@@ -35,6 +58,30 @@ class PlacesTableViewController: UITableViewController {
         } catch {
             print("Erro ao carregar locais")
         }
+    }
+    
+    func prepareShowAllBtn() {
+        if self.places.count > 0 {
+            let btn = UIBarButtonItem(title: "Mostrar todos no mapa", style: .plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btn
+        } else {
+            navigationItem.leftBarButtonItem = nil
+        }
+    }
+    
+    func prepareNoItems() {
+        if self.places.count <= 0 {
+            let lbNoItems = UILabel()
+            lbNoItems.text = "Nenhum local salvo ainda"
+            lbNoItems.textAlignment = .center
+            tableView.backgroundView = lbNoItems
+        } else {
+            tableView.backgroundView = nil
+        }
+    }
+    
+    @objc func showAll() {
+        performSegue(withIdentifier: "segueToMap", sender: nil)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -59,8 +106,6 @@ extension PlacesTableViewController: FindPlaceDelegate {
     func savePlace(_ place: Place) {
         guard !self.places.contains(place) else {return}
         self.places.append(place)
-        self.tableView.reloadData()
-        let json = try? JSONEncoder().encode(self.places)
-        UserDefaults.standard.set(json, forKey: "places")
+        self.savePlaces()
     }
 }
